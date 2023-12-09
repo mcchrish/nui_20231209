f9359d5be6b0acebc8eb503516062a64e630a395
Michael Chris Lopez <hello@michaelchris.space>
2023-12-09 11:55:24 +0100

    parse firebase data and basic API validation

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,12 +5,56 @@ import fastify from "fastify";
 const server = fastify().withTypeProvider<JsonSchemaToTsProvider>();
 await server.register(cors, {});
 
+export function isQuestionData(data: unknown): data is QuestionData {
+  return typeof data === "object" && data !== null && "questions" in data;
+}
+
+interface Question {
+  answers: Record<string, Answer>;
+  title: string[];
+  title_alt: string[];
+}
+
+interface Answer {
+  result: string;
+  title: string;
+  title_alt: string;
+}
+
+interface QuestionData {
+  questions: Record<string, Question>;
+  topics: Record<string, { rule: string; title: Record<string, string> }>;
+}
+
 server.route({
   method: "POST",
   url: "/next-question",
   async handler(request) {
-    console.log("answers:", request.body);
-    return request.body;
+    const { answers } = request.body;
+    const res = await fetch(
+      "https://nui-testchallenge-default-rtdb.europe-west1.firebasedatabase.app/services_backend.json",
+    );
+    const data = await res.json();
+
+    if (!isQuestionData(data)) {
+      throw new Error("Invalid data");
+    }
+
+    const questions = Object.entries(data.questions);
+    const answeredKeys = answers.map((answer) => answer.key);
+
+    for (const question of questions) {
+      if (!answeredKeys.includes(question[0])) {
+        return {
+          nextQuestion: {
+            key: question[0],
+            title: question[1].title,
+            options: question[1].answers,
+          },
+        };
+      }
+    }
+    return { services: [] };
   },
   schema: {
     body: {
@@ -21,13 +65,40 @@ server.route({
           items: {
             type: "object",
             properties: {
-              key: "string",
-              answer: "string",
+              key: { type: "string" },
+              answer: { type: "string" },
             },
+            required: ["key", "answer"],
           },
         },
       },
       required: ["answers"],
+    },
+
+    response: {
+      200: {
+        type: "object",
+        properties: {
+          nextQuestion: {
+            type: "object",
+            properties: {
+              key: { type: "string" },
+              title: { type: "string" },
+              options: {
+                type: "array",
+                items: {
+                  type: "object",
+                  properties: {
+                    title: { type: "string" },
+                    result: { type: "string" },
+                  },
+                },
+              },
+            },
+          },
+          services: { type: "array", items: { type: "string" } },
+        },
+      },
     },
   },
 });
--- server/tests/api.spec.ts
+++ server/tests/api.spec.ts
@@ -2,9 +2,10 @@ import { test, expect } from "@playwright/test";
 
 test("next-question", async ({ request }) => {
   const data = {
-    answers: [{}],
+    answers: [],
   };
   const res = await request.post("/next-question", { data });
+  console.log(await res.json());
   expect(res.ok()).toBeTruthy();
   // expect(await res.json()).toEqual(data);
 });
